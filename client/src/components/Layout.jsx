de75982d69123fe487c94984ff8f94181990d080
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-05-14 11:23:36 -0700

    added header logic to recognize cookies

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,11 +5,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout() {
+  const [cookies, setCookies, removeCookie] = useCookies([""]);
+  const username = cookies.username ? `${cookies.username}` : ":username";
+  
+  function handleRemoveCookies(name) {
+    removeCookie(`${name}`, { path: "/" })
+  }
 
   return(
     <main>
       <div id="content-wrapper">
-        <Header />
+        <Header user_name={username} cookies={cookies} removeCookies={handleRemoveCookies}/>
         <Outlet />
       </div>
       <Footer />
