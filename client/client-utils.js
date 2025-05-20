3a3f63c1dfe5f32fb32688e0a68ea85d7dabc0b7
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-05-19 18:13:44 -0700

    fixing database

--- client/client-utils.js
+++ client/client-utils.js
@@ -2,7 +2,7 @@
 export async function sendData(route, allData) {
 
   try {
-    const response = await fetch(`https://fittrack-server-api.onrender.com/${route}`, {
+    const response = await fetch(`https://fittrack-server-api.onrender.com/api/${route}`, {
     method: "POST",
     credentials: "include",
     headers: {
