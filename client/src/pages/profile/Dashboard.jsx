98d57c6546bcd1368ef54a9a450fd86c4f888e2f
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-06-17 21:15:56 -0700

    trying

--- client/src/pages/profile/Dashboard.jsx
+++ client/src/pages/profile/Dashboard.jsx
@@ -245,7 +245,7 @@ export default function Dashboard() {
   const todaysSchedule = plannedWorkout ? 
   plannedWorkout.map((workouts) => {
     console.log(workouts.date)
-    console.log(formatLocalCurrentDate(new Date(`${workouts.date}`)))
+    console.log(formatCurrentDate(new Date(showDate)))
     return (                                                                                                                                                                    
       <div key={workouts.id} className={`workout-tbody-row workout-tbody-row-${refCount.current = refCount.current + 1}`}>
         <button className="workout-actions" formMethod="DELETE" name={`submit-individual`} value={workouts.id}>
@@ -259,7 +259,7 @@ export default function Dashboard() {
           className="displayDate"
           placeholder=""
           type="hidden"
-          value={formatLocalCurrentDate(new Date(`${workouts.date}`))}
+          value={formatCurrentDate(new Date(showDate))}
         />
 
         <label htmlFor="exerciseId"/>
