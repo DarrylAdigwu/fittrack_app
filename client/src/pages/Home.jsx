aa33b741cb0a29acfd140436fe8d1e92111c4493
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-06-25 11:58:22 -0700

    added an image to the home page, and changed styling to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router";
 import "../assets/css/home.css";
+import phoneMock from "../assets/images/mock-phone.png"
 
 export default function Home() {
   return(
-      <div className="container home-container">
-        <div className="home-bg">
-          <div className="overlay"></div>
-        </div>
-        <section className="main home-cta">
-        <h1>Make planning feel easy</h1>
+    <div className="container home-container">
+      <div className="home-bg">
+        <div className="overlay"></div>
+      </div>
+      <section className="home-cta">
+        <div className="cta-texts">
+          <h1>Make planning feel easy</h1>
           <p>
             FitTrack is your reliable workout partner. Start 
             planning and tracking your daily 
             fitness routines.
           </p>
           <NavLink to="register">Get Started</NavLink>
-        </section>
-      </div>
+        </div>
+        <div className="mock-img">
+          <img className="phone-mock-img" src={phoneMock} alt="mock up of site on iphone"/>
+        </div>
+      </section>
+    </div>
   )
 }
